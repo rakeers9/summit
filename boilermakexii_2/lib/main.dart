bbead8b24668f0f7af54d97b89607b5936694297
RhinoInani <69125015+RhinoInani@users.noreply.github.com>
2025-02-22 02:32:57 -0500

    Bottom Nav Bar + Work on profile

--- boilermakexii_2/lib/main.dart
+++ boilermakexii_2/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:boilermakexii_2/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider_plus/carousel_slider_plus.dart';
+
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,104 +23,67 @@ class MyApp extends StatelessWidget {
         textTheme: const TextTheme(
           bodyLarge: TextStyle(color: Colors.white),
           bodyMedium: TextStyle(color: Colors.white),
-          bodySmall:  TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MainScreen(),
     );
   }
 }
 
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-];
-
-
-final List<Widget> imageSliders = imgList.map((item) {
-  return Container(
-    margin: const EdgeInsets.all(5.0),
-    child: ClipRRect(
-      borderRadius: const BorderRadius.all(Radius.circular(30)),
-      child: Image.network(item, fit: BoxFit.cover, width: 300, height: 300,),
-    ),
-  );
-}).toList();
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  static final List<Widget> _pages = <Widget>[
+    HomePage(),
+    StartPage(),
+    UserProfile(
+      userName: "Prickle Peanuts",
+      profileImageUrl: "https://picsum.photos/200",
+    ),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double trophiesFontsize = size.width * 0.05;
-
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Trophies", style: TextStyle(fontSize: trophiesFontsize),),
-                    Text("23", style: TextStyle(fontSize: trophiesFontsize),),
-                  ],
-                ),
-              ),
-              CarouselSlider(
-                options: CarouselOptions(
-                  aspectRatio: 3/3,
-                  enlargeCenterPage: true,
-                  enlargeStrategy: CenterPageEnlargeStrategy.zoom,
-                  enlargeFactor: 0.3,
-                ),
-                items: imageSliders,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
-                child: SizedBox(
-                  width: size.width,
-                  height: size.height * 0.07,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      backgroundColor: Colors.amberAccent,
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                    ),
-                    onPressed: () {
-
-                    },
-                    child: Text(
-                      "Get Started",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: size.height * 0.02,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              InfoCard(imageUrl: 'https://picsum.photos/200', line1: "John McCormick", line2: "10", line3: "101"),
-              InfoCard(imageUrl: 'https://picsum.photos/200', line1: "Bob Dale", line2: "12", line3: "45"),
-              InfoCard(imageUrl: 'https://picsum.photos/200', line1: "Johnathan Crook", line2: "15", line3: "20"),
+      backgroundColor: Colors.black,
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Theme(
+          data: Theme.of(context).copyWith(canvasColor: Colors.black, splashColor: Colors.transparent, highlightColor: Colors.transparent,),
+          child: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.circle_outlined), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.person_outlined), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), label: ""),
             ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amberAccent,
+            selectedFontSize: 0,
+            unselectedFontSize: 0,
+            enableFeedback: true,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Colors.black,
+            iconSize: 30,
+            onTap: _onItemTapped,
           ),
         ),
       ),
@@ -125,46 +91,27 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-
-
-class InfoCard extends StatelessWidget {
-  final String imageUrl;
-  final String line1;
-  final String line2;
-  final String line3;
-
-  const InfoCard({
-    super.key,
-    required this.imageUrl,
-    required this.line1,
-    required this.line2,
-    required this.line3,
-  });
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.black,
-      elevation: 0,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(imageUrl),
-              radius: 30,
-            ),
-            SizedBox(width: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(line1, style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold)),
-                Text("Recent Cash Out: \$$line2", style: TextStyle(color: Colors.white, fontSize: 14)),
-                Text("Current Trophies: $line3", style: TextStyle(color: Colors.white, fontSize: 14)),
-              ],
-            ),
-          ],
-        ),
+    return Scaffold(
+      body: Center(
+        child: Text('Start Page', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Settings Page', style: TextStyle(color: Colors.white)),
       ),
     );
   }
--- boilermakexii_2/lib/profile.dart
+++ boilermakexii_2/lib/profile.dart
@@ -12,30 +12,129 @@ class UserProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(profileImageUrl),
-            backgroundColor: Colors.white,
+
+    Size size = MediaQuery.of(context).size;
+    int score = 56;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: size.height * 0.02),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(profileImageUrl),
+                  backgroundColor: Colors.white,
+                ),
+              ),
+              Text(
+                userName,
+                style: TextStyle(
+                  fontSize: size.height * 0.025,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+              Text(
+                "Bronze",
+                style: TextStyle(
+                  fontSize: size.height * 0.02,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "13th in Region",
+                style: TextStyle(
+                  fontSize: size.height * 0.017,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: size.height * 0.03),
+                child: Text(
+                  "\$32.00",
+                  style: TextStyle(
+                    fontSize: size.height * 0.05,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                "Current Score $score",
+                style: TextStyle(
+                  fontSize: size.height * 0.02,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
+                    child: SizedBox(
+                      width: size.width/3,
+                      height: size.height * 0.06,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          backgroundColor: Colors.amberAccent,
+                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                        ),
+                        onPressed: () {
+
+                        },
+                        child: Text(
+                          "Cash Out",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: size.height * 0.02,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
+                    child: SizedBox(
+                      width: size.width/3,
+                      height: size.height * 0.06,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          backgroundColor: Colors.amberAccent,
+                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                        ),
+                        onPressed: () {
+
+                        },
+                        child: Text(
+                          "Edit Profile",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: size.height * 0.02,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-          const SizedBox(height: 12),
-          Text(
-            userName,
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.amber,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
